9c0f0cd961c398efa7359d2374c7263fab30f523
t4r3k <142579274+machine-moon@users.noreply.github.com>
2024-07-04 05:18:32 +0000

    old changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,69 @@
 #include <iostream>
+#include <stdio.h>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+void printList(Node *n)
+{
+    if (n == nullptr)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    while (n != nullptr)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    printf("\n");
 }
 
+void addNode(Node *n, int data)
+{
+    Node *newNode = new Node();
+    newNode->data = data;
+    newNode->next = nullptr;
+
+    while (n->next != nullptr)
+    {
+        n = n->next;
+    }
+    n->next = newNode;
+}
+
+void removeLastNode(Node *n)
+{
+    while (n->next->next != nullptr)
+    {
+        n = n->next;
+    }
+    delete n->next;
+    n->next = nullptr;
+}
+
+int main()
+{
+    Node *head = new Node();
+    printList(head);
+    addNode(head, 1);
+    printList(head);
+    addNode(head, 2);
+    printList(head);
+    addNode(head, 3);
+
+    printList(head);
+
+    removeLastNode(head);
+    printList(head);
+    removeLastNode(head);
+    printList(head);
+    removeLastNode(head);
+    printList(head);
+
+    return 0;
+}
